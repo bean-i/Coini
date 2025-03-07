e454b04356d0c04e29f08210790fb92dbe7d7a8f
been <leebin0603@naver.com>
2025-03-07 20:54:00 +0900

    feat: Base 템플릿 구현

---
+++ Coini/Base/BaseCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  BaseCollectionViewCell.swift
+//  Coining
+//
+//  Created by 이빈 on 3/6/25.
+//
+
+import UIKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+    
+    func configureView() { }
+    
+}
---
+++ Coini/Base/BaseViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BaseViewController.swift
+//  Coining
+//
+//  Created by 이빈 on 3/6/25.
+//
+
+import UIKit
+
+class BaseViewController<T: UIView>: UIViewController {
+    
+    var mainView: T {
+        guard let customView = view as? T else {
+            fatalError("view는 \(T.self) 타입이어야 합니다.")
+        }
+        return customView
+    }
+    
+    override func loadView() {
+        self.view = T(frame: .zero)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureNavigation()
+        bind()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        view.backgroundColor = .white
+        navigationController?.navigationBar.topItem?.backButtonTitle = ""
+    }
+    
+    func configureNavigation() { }
+
+    func bind() { }
+}
