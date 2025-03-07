c0c873b33f373e4e3a5a06e525d521547ec44553
been <leebin0603@naver.com>
2025-03-07 20:47:57 +0900

    Initial Commit

---
+++ Coini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coini
+//
+//  Created by 이빈 on 3/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
