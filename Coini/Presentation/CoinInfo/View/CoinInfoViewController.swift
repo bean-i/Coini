3a153bb442ff9674f8972bfff4c1e6217f3f5796
been <leebin0603@naver.com>
2025-03-09 03:18:57 +0900

    feat: CoinInfo 화면 - 서치바 검색 기능 구현 SearchViewController로 화면 전환

--- Coini/Presentation/CoinInfo/View/CoinInfoViewController.swift
+++ Coini/Presentation/CoinInfo/View/CoinInfoViewController.swift
@@ -23,27 +23,44 @@ final class CoinInfoViewController: BaseViewController<CoinInfoView> {
     }
     
     override func bind() {
-        
         let input = CoinInfoViewModel.Input(viewDidLoadTrigger: Observable.just(0))
         let output = viewModel.transform(input: input)
 
+        // 인기 검색어
         output.coinItems
             .bind(to: mainView.keywordCollectionView.rx.items(cellIdentifier: KeywordCollectionViewCell.identifier, cellType: KeywordCollectionViewCell.self)) { (row, element, cell) in
                 cell.configureData(row: row, data: element)
             }
             .disposed(by: disposeBag)
         
+        // 인기 NFT
         output.NFTItems
             .bind(to: mainView.nftCollectionView.rx.items(cellIdentifier: NFTCollectionViewCell.identifier, cellType: NFTCollectionViewCell.self)) { (row, element, cell) in
                 cell.configureData(data: element)
             }
             .disposed(by: disposeBag)
         
+        // 조회 시간
         output.networkTime
             .map { DateFormatter.networkTime($0) }
             .bind(to: mainView.keywordDateLabel.rx.text)
             .disposed(by: disposeBag)
         
+        // 검색
+        
+        mainView.searchBar.rx.searchButtonClicked
+            .withUnretained(self)
+            .map { _ in self.mainView.searchBar.resignFirstResponder() }
+            .withLatestFrom(mainView.searchBar.rx.text.orEmpty)
+            .bind(with: self) { owner, value in
+                print(value)
+                if !value.trimmingCharacters(in: .whitespaces).isEmpty {
+                    // 화면 전환
+                    let vc = SearchViewController()
+                    owner.navigationController?.pushViewController(vc, animated: true)
+                }
+            }
+            .disposed(by: disposeBag)
     }
     
     
