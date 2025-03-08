da22d85e36e1479b378c8e9ab936f26740b12871
been <leebin0603@naver.com>
2025-03-08 18:18:08 +0900

    feat: CoinInfoViewController 구현 - 인기 검색어

---
+++ Coini/Presentation/CoinInfo/CoinInfoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CoinInfoViewController.swift
+//  Coini
+//
+//  Created by 이빈 on 3/8/25.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class CoinInfoViewController: BaseViewController<CoinInfoView> {
+    
+    let viewModel = CoinInfoViewModel()
+    let disposeBag = DisposeBag()
+    
+    override func configureNavigation() {
+        let titleLabel = UILabel()
+        titleLabel.text = "가상자산 / 심볼 검색"
+        titleLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        titleLabel.textColor = .customNavy
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
+    }
+    
+    override func bind() {
+        
+        let input = CoinInfoViewModel.Input(viewDidLoadTrigger: Observable.just(0))
+        let output = viewModel.transform(input: input)
+
+        output.coinItems
+            .bind(to: mainView.keywordCollectionView.rx.items(cellIdentifier: KeywordCollectionViewCell.identifier, cellType: KeywordCollectionViewCell.self)) { (row, element, cell) in
+                cell.configureData(row: row, data: element)
+            }
+            .disposed(by: disposeBag)
+        
+        output.networkTime
+            .map { DateFormatter.networkTime($0) }
+            .bind(to: mainView.keywordDateLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+    }
+    
+    
+}
