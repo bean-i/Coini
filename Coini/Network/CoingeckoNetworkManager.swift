d8803c2b7d80609c46a87713bf15b26516c3d90b
been <leebin0603@naver.com>
2025-03-08 18:16:35 +0900

    feat: CoinGeckoNetworkManager 구현 CoinGecko Trending API 통신

---
+++ Coini/Network/CoingeckoNetworkManager.swift
@@ -0,0 +1,49 @@
+//
+//  CoingeckoNetworkManager.swift
+//  Coini
+//
+//  Created by 이빈 on 3/8/25.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+import RxCocoa
+
+final class CoingeckoNetworkManager {
+    
+    static let shared = CoingeckoNetworkManager()
+    
+    private init() { }
+    
+    func getTrending<T: Decodable>(api: Router,
+                                  type: T.Type) -> Single<T> {
+        return Single.create { single in
+            guard let endpoint = api.endpoint else {
+                print("endpoint가 없음")
+                return Disposables.create {
+                    print("통신 종료")
+                }
+            }
+            
+            AF.request(endpoint,
+                       method: api.method,
+                       parameters: api.parameters,
+                       encoding: URLEncoding.queryString)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: T.self) { response in
+                switch response.result {
+                case .success(let value):
+                    single(.success(value))
+                case .failure(let error):
+                    print(error)
+                    single(.failure(error))
+                }
+            }
+            
+            return Disposables.create {
+                print("통신 종료")
+            }
+        }
+    }
+}
