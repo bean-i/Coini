fb81df09e10b2fc15f3bc345b0bdc78e09c766c5
been <leebin0603@naver.com>
2025-03-09 03:19:39 +0900

    feat: Search 화면 UI 구성

---
+++ Coini/Presentation/Search/SearchView.swift
@@ -0,0 +1,77 @@
+//
+//  SearchView.swift
+//  Coini
+//
+//  Created by 이빈 on 3/8/25.
+//
+
+import UIKit
+import SnapKit
+
+final class SearchView: BaseView {
+    
+    let backButton = UIBarButtonItem(
+        image: UIImage(systemName: "arrow.left"),
+        style: .plain,
+        target: nil,
+        action: nil
+    )
+    
+    let searchTextField = UITextField()
+    
+    let headerTabCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    let pageCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+//    let pageView = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        headerTabCollectionView.collectionViewLayout = configureHeaderTabCollectionViewLayout()
+    }
+    
+    override func configureHierarchy() {
+        addSubViews(
+            headerTabCollectionView,
+            pageCollectionView
+        )
+    }
+    
+    override func configureLayout() {
+        headerTabCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(44)
+        }
+        
+        pageCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(headerTabCollectionView.snp.bottom)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
+    }
+    
+    override func configureView() {
+        backButton.tintColor = .customNavy
+        
+        searchTextField.text = "Bitcoin"
+        searchTextField.borderStyle = .none
+        searchTextField.backgroundColor = .green
+        searchTextField.textAlignment = .left
+        
+        headerTabCollectionView.backgroundColor = .clear
+        headerTabCollectionView.showsHorizontalScrollIndicator = false
+        headerTabCollectionView.showsVerticalScrollIndicator = false
+        headerTabCollectionView.register(HeaderTabCollectionViewCell.self, forCellWithReuseIdentifier: HeaderTabCollectionViewCell.identifier)
+        
+        pageCollectionView.backgroundColor = .white
+    }
+    
+    private func configureHeaderTabCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewFlowLayout()
+        let deviceWidth = frame.width
+        let itemSize = deviceWidth / 3
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: itemSize, height: 44)
+        return layout
+    }
+}
