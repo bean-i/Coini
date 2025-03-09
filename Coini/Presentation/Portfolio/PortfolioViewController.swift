f9681231ce7ea45a6e19cbb984d64e79daced83f
been <leebin0603@naver.com>
2025-03-10 00:11:38 +0900

    feat: Portfolio 화면 - 타이틀 설정

--- Coini/Presentation/Portfolio/PortfolioViewController.swift
+++ Coini/Presentation/Portfolio/PortfolioViewController.swift
@@ -11,6 +11,15 @@ final class PortfolioViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureNavigation()
+    }
+    
+    private func configureNavigation() {
+        let titleLabel = UILabel()
+        titleLabel.text = "포트폴리오"
+        titleLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        titleLabel.textColor = .customNavy
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
     }
 
 }
