0a74bdc0031117c039b6878b0f2887e0c5e21e83
been <leebin0603@naver.com>
2025-03-10 18:24:16 +0900

    feat: CoinMarket API - 네트워크 모델 구현

---
+++ Coini/Presentation/SearchDetail/Model/CoinMarket.swift
@@ -0,0 +1,62 @@
+//
+//  CoinMarket.swift
+//  Coini
+//
+//  Created by 이빈 on 3/10/25.
+//
+
+import Foundation
+
+struct CoinMarket: Decodable {
+    let id: String
+    let name: String
+    let image: String
+    let currentPrice: Int
+    let priceChange24h: Double
+    let lastUpdate: String
+    let low24h: Int
+    let high24h: Int
+    let allTimeHighPrice: Int
+    let allTimeHighPriceDate: String
+    let allTimeLowPrice: Int
+    let allTimeLowPriceDate: String
+    let marketCap: Int
+    let fullyDilutedValue: Int
+    let totalVolume: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id, name, image
+        case currentPrice = "current_price"
+        case priceChange24h = "price_change_percentage_24h"
+        case lastUpdate = "last_updated"
+        case low24h = "low_24h"
+        case high24h = "high_24h"
+        case allTimeHighPrice = "ath"
+        case allTimeHighPriceDate = "ath_date"
+        case allTimeLowPrice = "atl"
+        case allTimeLowPriceDate = "atl_date"
+        case marketCap = "market_cap"
+        case fullyDilutedValue = "fully_diluted_valuation"
+        case totalVolume = "total_volume"
+    }
+}
+
+extension CoinMarket {
+    static let empty = CoinMarket(
+        id: "empty",
+        name: "empty",
+        image: "empty",
+        currentPrice: 0,
+        priceChange24h: 0,
+        lastUpdate: "empty",
+        low24h: 0,
+        high24h: 0,
+        allTimeHighPrice: 0,
+        allTimeHighPriceDate: "empty",
+        allTimeLowPrice: 0,
+        allTimeLowPriceDate: "empty",
+        marketCap: 0,
+        fullyDilutedValue: 0,
+        totalVolume: 0
+    )
+}
