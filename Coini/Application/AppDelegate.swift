66d1d274003771f5cb3140a59ca43a383762c80c
been <leebin0603@naver.com>
2025-03-11 21:07:21 +0900

    chore: iqkeyboard 추가

--- Coini/Application/AppDelegate.swift
+++ Coini/Application/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.isEnabled = true
+        IQKeyboardManager.shared.resignOnTouchOutside = true
         AppAppearance.configureAppearance()
         NetworkMonitor.shared.startMonitoring()
         return true
