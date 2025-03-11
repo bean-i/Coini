8577c3285938e12917f5a4e0e14751388a3101ce
been <leebin0603@naver.com>
2025-03-11 19:24:54 +0900

    feat: Coingecko 네트워크 - 에러 반환 추가

--- Coini/Network/CoingeckoNetworkManager.swift
+++ Coini/Network/CoingeckoNetworkManager.swift
@@ -20,7 +20,7 @@ final class CoingeckoNetworkManager {
                                   type: T.Type) -> Single<T> {
         return Single.create { single in
             guard let endpoint = api.endpoint else {
-                print("endpoint가 없음")
+                single(.failure(AFError.invalidURL))
                 return Disposables.create {
                     print("통신 종료")
                 }
@@ -32,12 +32,16 @@ final class CoingeckoNetworkManager {
                        encoding: URLEncoding.queryString)
             .validate(statusCode: 200..<300)
             .responseDecodable(of: T.self) { response in
+                print("🐊🐊CoinGecko API 통신!🐊🐊")
                 switch response.result {
                 case .success(let value):
                     single(.success(value))
                 case .failure(let error):
-                    print(error)
-                    single(.failure(error))
+                    if let code = error.responseCode {
+                        single(.failure(AFError.geckoError(code: code)))
+                    } else {
+                        single(.failure(AFError.afError))
+                    }
                 }
             }
             
